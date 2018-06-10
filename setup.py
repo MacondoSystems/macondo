6e4584175c1e5eedfe425354bc0af65145d04e28
Andres Root <info@andresroot.co>
2018-06-10 12:22:38 -0500

    WIP

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 
 import setuptools
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="macondo",
-    version="0.0.0.1",
+    name='macondo',
+    version='0.0.0.1',
     author='Andres Lujan',
     author_email='info@andresroot.co',
-    description="A CMS",
+    description='A CMS',
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type='text/markdown',
     url='https://github.com/MacondoSystems/macondo/',
     packages=['macondo'],
     keywords=['cms', 'django', 'privacy'],
     classifiers=(
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
     ),
 )
