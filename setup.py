e00afeb68e041b1ff9bc6bf11b1a0fcf549585d8
Andres Root <info@andresroot.co>
2018-06-10 12:03:14 -0500

    WIP

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
     keywords=['cms', 'django', 'privacy'],
     classifiers=(
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPLv3 License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ),
 )
