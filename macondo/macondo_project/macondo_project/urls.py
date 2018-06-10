d3c7eef4998333336de38103f25ff62d95c24365
Andres Root <info@andresroot.co>
2018-06-10 16:42:26 -0500

    WIP

--- macondo/macondo_project/macondo_project/urls.py
+++ macondo/macondo_project/macondo_project/urls.py
@@ -1,4 +1,4 @@
-"""macondo URL Configuration
+"""macondo_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
--- macondo/macondo_project/manage.py
+++ macondo/macondo_project/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "macondo.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "macondo_project.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
